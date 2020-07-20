b739564cc7d6fbcba3f0807d757c03dfe937a218
Nurgenius <belal.mnur@gmail.com>
2020-07-20 12:59:55 +0300

    Information/Guide File Added

---
+++ MAIN.py
@@ -0,0 +1,21 @@
+# This document will contain example functions that will be needed in this project
+# We will be using an API called OpenWeather to retrieve the weather data
+# The API key (which is needed to use the service) is "f3793d49603744dda6b31a3ee5adf22d"
+
+# Template for OpenWeather API GET https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&
+# exclude={part}&appid={YOUR API KEY}
+
+import requests
+import json
+
+# Example response with Doha, Qatar as the location
+
+w_retrieval = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=25.3&lon=51.1& exclude=minutely,hourly,daily&appid=f3793d49603744dda6b31a3ee5adf22d')
+
+print(w_retrieval.status_code)  # Prints the status code (200 = OK) (If the API isn't connecting properly, this is the first step to troubleshoot)
+print(w_retrieval.json())       # Prints all the content of the API request (All the data)
+print("\n")
+
+w_log = w_retrieval.json()
+
+print("Temperature: " + str(w_log['current']['temp'])) # Prints the current temperature
