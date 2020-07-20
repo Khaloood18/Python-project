2504252b69ef062c4d79f88a93ad345cbc9e85dd
KhaloodAlWb <68500284+Khaloood18@users.noreply.github.com>
2020-07-20 15:56:41 +0300

    Some changes ...

--- MayaandDiala.py
+++ MayaandDiala.py
@@ -1,1 +1,9 @@
 import datetime,pytz
+for tz in pytz.all_timezones:
+    print (tz)
+
+local=input('Continent/Country from the above options? ')
+d = datetime.datetime.now()
+timezone = pytz.timezone(local)
+d_aware = timezone.localize(d)
+print(str(d_aware))
