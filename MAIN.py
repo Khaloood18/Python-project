e40699fd2543d329d6b589f79ecbe1eb22cd8724
Nurgenius <belal.mnur@gmail.com>
2020-07-20 14:13:04 +0300

    Updated MeandBelal.py Made small edits to MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -12,7 +12,7 @@ import requests
 w_retrieval = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=25.3&lon=51.1& exclude=minutely,hourly,daily&appid=f3793d49603744dda6b31a3ee5adf22d')
 
 print(w_retrieval.status_code)  # Prints the status code (200 = OK) (If the API isn't connecting properly, this is the first step to troubleshoot)
-print(w_retrieval.json())       # Prints all the content of the API request (All the data)
+#print(w_retrieval.json())       # Prints all the content of the API request (All the data)
 print("\n")
 
 w_log = w_retrieval.json()      # Store the json file (the json file contains all the data, it's like a neat box of information that OpenWeather and other API's give is)
