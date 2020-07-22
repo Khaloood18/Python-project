7a50f18f6425fb8acf0f5ff05ecbe65a83b9dd67
Nurgenius <belal.mnur@gmail.com>
2020-07-22 12:07:50 +0300

    Updated MeandBelal.py

--- MeandBelal.py
+++ MeandBelal.py
@@ -1,24 +1,34 @@
+# We will be using an API called OpenWeather to retrieve the weather data
+# The API key (which is needed to use the service) is "f3793d49603744dda6b31a3ee5adf22d"
+
 import requests
 
 apiKey = 'f3793d49603744dda6b31a3ee5adf22d'
 
-
 class weatherLog:
     def __init__(self):
-        self.cityLat = 0
-        self.cityLong = 0
+        self.cityName = ''
     def setInfo(self):
-        print("Type in the Latitude")
-        self.cityLat = input()
-        print("Type in the Longitude")
-        self.cityLong = input()
-        cityLog = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + self.cityLat + '&lon=' + self.cityLong + '& exclude=minutely,hourly,daily&units=metric&appid=' + apiKey)
-        self.logJson = cityLog.json()
+        print("Type in a city name:")
+        while True:
+            cityName = input()
+            cityFind = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&exclude=minutely,hourly,daily&units=metric&appid=' + apiKey)
+            cityFindJson = cityFind.json()
+            if (str(cityFind.status_code) == '200'):
+                print("City Found!")
+                self.cityLog = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + str(cityFindJson['coord']['lat']) + '&lon=' + str(cityFindJson['coord']['lon']) + '&exclude=minutely,hourly,daily&units=metric&appid=' + apiKey)
+                break
+            print("That is not a valid city, type in a city name:")
+        self.logJson = self.cityLog.json()
     def getTemp(self):
-        self.cityTemp = "Temperature: " + str(self.logJson['current']['temp'])
+        self.cityTemp = "Current Temperature: " + str(self.logJson['current']['temp'])
         return self.cityTemp
+    def getHumidity(self):
+        self.cityHumidity = " Humidity: " + str(self.logJson['current']['humidity'])
+        return self.cityHumidity
 
 weatherA = weatherLog()
 weatherA.setInfo()
 print(weatherA.getTemp())
+print(weatherA.getHumidity())
 
